9ef57fe2e79fca986188fa08d038c6d6130dc166
Valtersystem <valtersantos.ifsp@gmail.com>
2023-10-30 17:27:02 +0000

    .

---
+++ resources/js/Components/Nav.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex h-screen flex-col justify-between items-center border-e bg-white">
+        <div class="px-4 py-6 flex flex-col items-center">
+
+            <img src="../../../public/imagens/ka.png" alt="logo" class="w-20">
+
+            <ul class="mt-6 space-y-1">
+
+                <li>
+                    <a href=""
+                        class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-pink-900 hover:text-white">
+                        Products
+                    </a>
+                </li>
+
+                <li>
+                    <a href=""
+                        class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-pink-900 hover:text-white">
+                        Categories
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
